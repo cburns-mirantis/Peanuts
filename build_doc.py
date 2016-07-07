acf292bdb34651e7879470339a857373a84cfd69
Ramon Melero <rmelero@mirantis.com>
2016-07-07 18:18:13 -0500

    removed ssh dependancy and handled token through api.

--- build_doc.py
+++ build_doc.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-import zipfile,time,argparse,paramiko,requests,json
+import zipfile,time,argparse,requests,json,sys
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 parser = argparse.ArgumentParser(description='Gather Fuel Screenshots')
-parser.add_argument('-c', "--customer", action="store", dest="customer", type=str, help='Customer Name',required=True)
-parser.add_argument('-u', "--username", action="store", dest="username", type=str, help='SSH Username',required=True)
-parser.add_argument('-p', "--password", action="store", dest="password", type=str, help='SSH Password',required=True)
+parser.add_argument('-u', "--username", action="store", dest="username", type=str, help='Fuel Username',default="admin")
+parser.add_argument('-p', "--password", action="store", dest="password", type=str, help='Fuel Password',default="admin")
 parser.add_argument('-f', "--fuel", action="store", dest="host", type=str, help='Fuel FQDN or IP Ex. 10.20.0.2',required=True)
 args = parser.parse_args()
 
 def get_token():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(args.host, username=args.username, password=args.password)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("fuel token")
-    return ssh_stdout.readlines()[0]
+    header = {"Content-Type": "application/json", 'accept': 'application/json'}
+    creds = {"auth": {"tenantName": args.username,"passwordCredentials": {"username": args.username,"password": args.password}}}
+    r = requests.post(url="https://" + args.host + ":8443/keystone/v2.0/tokens",headers=header,verify=False,json=creds)
+    if r.status_code is not 200:
+        sys.exit("Check Fuel username & password")
+    return json.loads(r.text)['access']['token']['id']
 
 def get_nodes():
-    header = {"X-Auth-Token": get_token(),
-              "Content-Type": "application/json"}
-    return json.loads(requests.get("https://" + args.host + ":8443/api/nodes",headers=header, verify=False).text)
+    header = {"X-Auth-Token": get_token(),"Content-Type": "application/json"}
+    return json.loads(requests.get(url="https://" + args.host + ":8443/api/nodes",headers=header, verify=False).text)
 
 def docx_replace(old_file,new_file,rep):
     zin = zipfile.ZipFile (old_file, 'r')
@@ -39,7 +38,10 @@ def docx_replace(old_file,new_file,rep):
 nodes = get_nodes()
 
 replace = {
-"CUSTOMER" : args.customer,
+"CUSTOMER" : "Big Customer",
+"ENV" : "Big Customer",
+"RELEASE" : "Big Customer",
+"CUSTOMER" : "Big Customer",
 "TOTAL_NODES" : len(nodes),
 "DATE" : time.strftime("%d/%m/%Y")
 
