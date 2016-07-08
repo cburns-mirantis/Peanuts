ea0eae75bedd0bde6706fd6e48f31f3d8162f2d7
Ramon Melero <rmelero@mirantis.com>
2016-07-08 18:05:26 -0500

    added fuel node info gather

--- build_doc.py
+++ build_doc.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
-import zipfile,time,argparse,requests,json,sys
+
+# Mangement Interface
+# Get DHCP Range
+# Split document into different sections
+
+# Post creation report or data output
+# store report in a per run basis
+
+import zipfile,time,argparse,requests,json,sys,os,paramiko
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+# Handle Arguments
 parser = argparse.ArgumentParser(description='Gather Fuel Screenshots')
 parser.add_argument('-u', "--username", action="store", dest="username", type=str, help='Fuel Username',default="admin")
 parser.add_argument('-p', "--password", action="store", dest="password", type=str, help='Fuel Password',default="admin")
+parser.add_argument('-su', "--ssh-user", action="store", dest="ssh_username", type=str, help='SSH Username',default="root")
+parser.add_argument('-sp', "--ssh-pw", action="store", dest="ssh_password", type=str, help='SSH Password',default="r00tme")
 parser.add_argument('-f', "--fuel", action="store", dest="host", type=str, help='Fuel FQDN or IP Ex. 10.20.0.2',required=True)
 args = parser.parse_args()
 
+def fuel_info():
+    ssh = paramiko.SSHClient()
+    fuel = {}
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(args.host, username=args.ssh_username, password=args.ssh_password)
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("ip route ls | grep " + args.host + "| awk -e '{ print $3 }'")
+    fuel['mangement_iface'] = ssh_stdout.readlines()[0].replace("\n","")
+
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("route -n | grep UG | awk -e '{ print $2 }'")
+    fuel['gateway'] = ssh_stdout.readlines()[0].replace("\n","")
+
+    fuel['url'] = "https://" + args.host + ":8443"
+
+    fuel['ssh'] = {"username":args.ssh_username,"password":args.ssh_password}
+    fuel['web'] = {"username":args.username,"password":args.password}
+
+    return fuel
+
+# Get token from Keystone
 def get_token():
     header = {"Content-Type": "application/json", 'accept': 'application/json'}
     creds = {"auth": {"tenantName": args.username,"passwordCredentials": {"username": args.username,"password": args.password}}}
@@ -17,10 +48,12 @@ def get_token():
         sys.exit("Check Fuel username & password")
     return json.loads(r.text)['access']['token']['id']
 
-def get_nodes():
-    header = {"X-Auth-Token": get_token(),"Content-Type": "application/json"}
+# Get nodes from Fuel API
+def get_nodes(token):
+    header = {"X-Auth-Token": token,"Content-Type": "application/json"}
     return json.loads(requests.get(url="https://" + args.host + ":8443/api/nodes",headers=header, verify=False).text)
 
+# Replaces items in the docx
 def docx_replace(old_file,new_file,rep):
     zin = zipfile.ZipFile (old_file, 'r')
     zout = zipfile.ZipFile (new_file, 'w')
@@ -35,17 +68,19 @@ def docx_replace(old_file,new_file,rep):
     zout.close()
     zin.close()
 
-nodes = get_nodes()
+if not os.path.exists("docs"):
+    os.makedirs("docs")
 
-replace = {
-"CUSTOMER" : "Big Customer",
-"ENV" : "Big Customer",
-"RELEASE" : "Big Customer",
-"CUSTOMER" : "Big Customer",
-"TOTAL_NODES" : len(nodes),
-"DATE" : time.strftime("%d/%m/%Y")
+print(fuel_info())
 
-}
 
-print (replace)
-# docx_replace("template.docx","runbook.docx",replace)
+# token = get_token()
+# nodes = get_nodes(token)
+# entries = json.load(open("entries.json"))
+# entries['DATE'] = time.strftime("%d/%m/%Y")
+
+# docx_replace("cover.docx","docs/1.cover.docx",entries)
+#
+# replace = {
+# "TOTAL_NODES" : len(nodes)
+# }
