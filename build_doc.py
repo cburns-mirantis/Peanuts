99ea7fcf67c28d2546cf577833671900e891eb19
Matt Schafer <mschafer@mirantis.com>
2016-07-11 15:57:27 -0700

    Moving BuildVMsTable.py code into build_doc.py

--- build_doc.py
+++ build_doc.py
@@ -10,6 +10,9 @@
 import zipfile,time,argparse,requests,json,sys,os,paramiko
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from docx import Document
+from docx.shared import Inches
+from docx.enum.style import WD_STYLE
 
 # Handle Arguments
 parser = argparse.ArgumentParser(description='Gather Fuel Screenshots')
@@ -70,6 +73,41 @@ def docx_replace(old_file,new_file,rep):
 if not os.path.exists("docs"):
     os.makedirs("docs")
 
+def gen_nodes_table(nodeData):
+	# Generate row_count by counting the number of nodes and adding 1 for the header row
+	row_count = len(nodeData)+1
+	col_count = 6
+
+	# Create (technically open) a new document
+	doc = Document()
+
+	# Add a heading; last digit is heading size
+	doc.add_heading('Nodes(VMs)',1)
+
+	# Create table for the data
+	table = doc.add_table(row_count, col_count)
+	table.style = 'TableGrid'
+
+	# Generate the header row for the NodesVMS table
+	hdr_cells = table.rows[0].cells
+	hdr_cells[0].text = 'Hostname'
+	hdr_cells[1].text = 'Role(s)'
+	hdr_cells[2].text = 'Admin network IP address'
+	hdr_cells[3].text = 'CPUxCores'
+	hdr_cells[4].text = 'RAM'
+	hdr_cells[5].text = 'HDD'
+
+	# Modify each additional row using data for the node
+	for nodeCounter, node in enumerate(nodeData):
+		nodeRow = table.rows[nodeCounter+1].cells
+		nodeRow[0].text = nodeData[nodeCounter]['hostname']
+		nodeRow[1].text = {x+' ' for x in nodeData[nodeCounter]['roles']}
+		nodeRow[2].text = nodeData[nodeCounter]['ip']
+		nodeRow[3].text = str(nodeData[nodeCounter]['meta']['cpu']['total']) # Total or real?
+		nodeRow[4].text = str(int(nodeData[nodeCounter]['meta']['memory']['total']/1048576)) + ' MB' # Total or max cap?
+		nodeRow[5].text = {str(x['disk']) + ': ' + str(int(x['size']/1073741824)) + 'GB \n' for x in nodeData[nodeCounter]['meta']['disks']}
+	doc.save('5.docs/NodesVMs.docx')
+
 # 1.cover
 entries = json.load(open("entries.json"))
 entries['DATE'] = time.strftime("%d/%m/%Y")
@@ -103,6 +141,8 @@ print("Built docs/6.access.docx")
 
 token = get_token()
 nodes = get_nodes(token)
+gen_nodes_table(nodes)
+
 #
 replace = {
 "TOTAL_NODES" : len(nodes)
