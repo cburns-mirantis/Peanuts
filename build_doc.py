69a2ea46037d265f4422b8bb636004e759a4dff4
Ramon Melero <rmelero@mirantis.com>
2016-07-11 13:06:22 -0500

    working doc replace

--- build_doc.py
+++ build_doc.py
@@ -13,8 +13,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # Handle Arguments
 parser = argparse.ArgumentParser(description='Gather Fuel Screenshots')
-parser.add_argument('-u', "--username", action="store", dest="username", type=str, help='Fuel Username',default="admin")
-parser.add_argument('-p', "--password", action="store", dest="password", type=str, help='Fuel Password',default="admin")
+parser.add_argument('-u', "--web-user", action="store", dest="web_username", type=str, help='Fuel Username',default="admin")
+parser.add_argument('-p', "--web-pw", action="store", dest="web_password", type=str, help='Fuel Password',default="admin")
 parser.add_argument('-su', "--ssh-user", action="store", dest="ssh_username", type=str, help='SSH Username',default="root")
 parser.add_argument('-sp', "--ssh-pw", action="store", dest="ssh_password", type=str, help='SSH Password',default="r00tme")
 parser.add_argument('-f', "--fuel", action="store", dest="host", type=str, help='Fuel FQDN or IP Ex. 10.20.0.2',required=True)
@@ -25,24 +25,23 @@ def fuel_info():
     fuel = {}
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(args.host, username=args.ssh_username, password=args.ssh_password)
-
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("ip route ls | grep " + args.host + "| awk -e '{ print $3 }'")
     fuel['mangement_iface'] = ssh_stdout.readlines()[0].replace("\n","")
-
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("route -n | grep UG | awk -e '{ print $2 }'")
     fuel['gateway'] = ssh_stdout.readlines()[0].replace("\n","")
+    # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("fuel plugins list")
+    # print(ssh_stdout.readlines())
 
     fuel['url'] = "https://" + args.host + ":8443"
-
     fuel['ssh'] = {"username":args.ssh_username,"password":args.ssh_password}
-    fuel['web'] = {"username":args.username,"password":args.password}
-
+    fuel['web'] = {"username":args.web_username,"password":args.web_password}
+    fuel['horizon'] = "172.16.0.2" # get horizon address
     return fuel
 
 # Get token from Keystone
 def get_token():
     header = {"Content-Type": "application/json", 'accept': 'application/json'}
-    creds = {"auth": {"tenantName": args.username,"passwordCredentials": {"username": args.username,"password": args.password}}}
+    creds = {"auth": {"tenantName": args.web_username,"passwordCredentials": {"username": args.web_username,"password": args.web_password}}}
     r = requests.post(url="https://" + args.host + ":8443/keystone/v2.0/tokens",headers=header,verify=False,json=creds)
     if r.status_code is not 200:
         sys.exit("Check Fuel username & password")
@@ -71,16 +70,44 @@ def docx_replace(old_file,new_file,rep):
 if not os.path.exists("docs"):
     os.makedirs("docs")
 
-print(fuel_info())
+# 1.cover
+entries = json.load(open("entries.json"))
+entries['DATE'] = time.strftime("%d/%m/%Y")
+docx_replace("templates/1.cover.docx","docs/1.cover.docx",entries)
+print("Built docs/1.cover.docx")
 
+# 2.intro
 
-# token = get_token()
-# nodes = get_nodes(token)
-# entries = json.load(open("entries.json"))
-# entries['DATE'] = time.strftime("%d/%m/%Y")
+# print("Built docs/2.intro.docx")
+# 3.architecture
 
-# docx_replace("cover.docx","docs/1.cover.docx",entries)
+# print("Built docs/3.architecture.docx")
+# 4.network
+
+# print("Built docs/4.network.docx")
+# 5.access
+fuel = fuel_info()
+access = {
+"FUELURL": fuel['url'],
+"FUELIP": args.host,
+"WEBUSER": fuel['web']['username'],
+"WEBPW": fuel['web']['password'],
+"HORURL": fuel['horizon'],
+"SSHUSER": fuel['ssh']['username'],
+"SSHUSER": fuel['ssh']['username'],
+"SSHPW": fuel['ssh']['password']
+}
+docx_replace("templates/6.access.docx","docs/6.access.docx",access)
+print("Built docs/6.access.docx")
+# 6.fuel
+
+token = get_token()
+nodes = get_nodes(token)
 #
-# replace = {
-# "TOTAL_NODES" : len(nodes)
-# }
+replace = {
+"TOTAL_NODES" : len(nodes)
+}
+for n in nodes:
+    print( n['hostname'])
+
+# print("Built docs/6.fuel.docx")
