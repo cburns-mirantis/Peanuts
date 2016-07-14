63bb9698f5bc5131946b4fcc1f307be59b47cfd6
Matt Schafer <mschafer@mirantis.com>
2016-07-14 13:24:06 -0700

    Adding function to generate access table with python-docx

--- build_doc.py
+++ build_doc.py
@@ -25,14 +25,58 @@ parser.add_argument('-su', "--ssh-user", action="store", dest="ssh_username", ty
 parser.add_argument('-sp', "--ssh-pw", action="store", dest="ssh_password", type=str, help='SSH Password',default="r00tme")
 parser.add_argument('-f', "--fuel", action="store", dest="host", type=str, help='Fuel FQDN or IP Ex. 10.20.0.2',required=True)
 args = parser.parse_args()
+def gen_access_table(fuel):
+   row_count = 8
+   col_count = 3
 
+   doc = Document()
+
+   doc.add_heading("Access Information",0)
+
+   table = doc.add_table(row_count, col_count)
+   table.style = doc.styles['Table Grid']
+
+   hdr_cells = table.rows[0].cells
+   hdr_cells[0].text = 'Host'
+   hdr_cells[1].text = 'IP address'
+   hdr_cells[2].text = 'Comment(s)'
+
+   line1 = table.rows[1].cells
+   line1[0].text = 'Fuel UI Master Node URL'
+   line1[1].text = fuel['url']
+
+   line2 = table.rows[2].cells
+   line2[0].text = 'Fuel UI Credentials'
+   line2[1].text = fuel['web']['username'] + ' / ' + fuel['web']['password']
+
+   line3 = table.rows[3].cells
+   line3[0].text = 'Fuel Master Node IP'
+   line3[1].text = args.host
+
+   line4 = table.rows[4].cells
+   line4[0].text = 'Fuel SSH Credentials (console)'
+   line4[1].text = fuel['ssh']['username'] + ' / ' + fuel['ssh']['password']
+
+   line5 = table.rows[5].cells
+   line5[0].text = 'OpenStack Nodes SSH Credentials (console)'
+   #line5[1].text = fuel['ssh']['username'] + ' / ' + fuel['ssh']['password']
+
+   line6 = table.rows[6].cells
+   line6[0].text = 'OpenStack Horizon URL'
+   line6[1].text = fuel['horizon']
+
+   line7 = table.rows[7].cells
+   line7[0].text = 'OpenStack Credentials'
+   line7[1].text = fuel['web']['username'] + ' / ' + fuel['web']['password']
+
+   doc.save("docs/6.access.docx")
 def fuel_info():
     ssh = paramiko.SSHClient()
     fuel = {}
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(args.host, username=args.ssh_username, password=args.ssh_password)
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("ip route ls | grep " + args.host + "| awk -e '{ print $3 }'")
-    fuel['mangement_iface'] = ssh_stdout.readlines()[0].replace("\n","")
+    fuel['management_iface'] = ssh_stdout.readlines()[0].replace("\n","")
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("route -n | grep UG | awk -e '{ print $2 }'")
     fuel['gateway'] = ssh_stdout.readlines()[0].replace("\n","")
     # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("fuel plugins list")
@@ -93,84 +137,84 @@ if not os.path.exists("docs"):
     os.makedirs("docs")
 
 def gen_nodes_table(nodeData):
-	# Generate row_count by counting the number of nodes and adding 1 for the header row
-	row_count = len(nodeData)+1
-	col_count = 6
+   # Generate row_count by counting the number of nodes and adding 1 for the header row
+   row_count = len(nodeData)+1
+   col_count = 6
 
-	# Create (technically open) a new document
-	doc = Document()
+   # Create (technically open) a new document
+   doc = Document()
 
-	# Add a heading; last digit is heading size
-	doc.add_heading('Nodes(VMs)',1)
+   # Add a heading; last digit is heading size
+   doc.add_heading('Nodes(VMs)',1)
 
-	# Create table for the data
-	table = doc.add_table(row_count, col_count)
-	table.style = doc.styles['Table Grid']
+   # Create table for the data
+   table = doc.add_table(row_count, col_count)
+   table.style = doc.styles['Table Grid']
 
-	# Generate the header row for the NodesVMS table
-	hdr_cells = table.rows[0].cells
-	hdr_cells[0].text = 'Hostname'
-	hdr_cells[1].text = 'Role(s)'
-	hdr_cells[2].text = 'Admin network IP address'
-	hdr_cells[3].text = 'CPUxCores'
-	hdr_cells[4].text = 'RAM'
-	hdr_cells[5].text = 'HDD'
+   # Generate the header row for the NodesVMS table
+   hdr_cells = table.rows[0].cells
+   hdr_cells[0].text = 'Hostname'
+   hdr_cells[1].text = 'Role(s)'
+   hdr_cells[2].text = 'Admin network IP address'
+   hdr_cells[3].text = 'CPUxCores'
+   hdr_cells[4].text = 'RAM'
+   hdr_cells[5].text = 'HDD'
 
-	# Modify each additional row using data for the node
-	for nodeCounter, node in enumerate(nodeData):
-		nodeRow = table.rows[nodeCounter+1].cells
-		nodeRow[0].text = nodeData[nodeCounter]['hostname']
-		nodeRow[1].text = {x+' ' for x in nodeData[nodeCounter]['roles']}
-		nodeRow[2].text = nodeData[nodeCounter]['ip']
-		nodeRow[3].text = str(nodeData[nodeCounter]['meta']['cpu']['total']) # Total or real?
-		nodeRow[4].text = str(int(nodeData[nodeCounter]['meta']['memory']['total']/1048576)) + ' MB' # Total or max cap?
-		nodeRow[5].text = {str(x['disk']) + ': ' + str(int(x['size']/1073741824)) + 'GB \n' for x in nodeData[nodeCounter]['meta']['disks']}
-	doc.save('docs/5.nodes.docx')
+   # Modify each additional row using data for the node
+   for nodeCounter, node in enumerate(nodeData):
+      nodeRow = table.rows[nodeCounter+1].cells
+      nodeRow[0].text = nodeData[nodeCounter]['hostname']
+      nodeRow[1].text = {x+' ' for x in nodeData[nodeCounter]['roles']}
+      nodeRow[2].text = nodeData[nodeCounter]['ip']
+      nodeRow[3].text = str(nodeData[nodeCounter]['meta']['cpu']['total']) # Total or real?
+      nodeRow[4].text = str(int(nodeData[nodeCounter]['meta']['memory']['total']/1048576)) + ' MB' # Total or max cap?
+      nodeRow[5].text = {str(x['disk']) + ': ' + str(int(x['size']/1073741824)) + 'GB \n' for x in nodeData[nodeCounter]['meta']['disks']}
+   doc.save('docs/5.nodes.docx')
 
 def gen_network_layout_table(networkData):
-	row_count = len(networkData)+1
-	col_count = 6
+   row_count = len(networkData)+1
+   col_count = 6
 
-	doc = Document()
+   doc = Document()
 
-	doc.add_heading("Network Layout",0)
+   doc.add_heading("Network Layout",0)
 
-	table = doc.add_table(row_count, col_count)
-	table.style = doc.styles['Table Grid']
+   table = doc.add_table(row_count, col_count)
+   table.style = doc.styles['Table Grid']
 
-	hdr_cells = table.rows[0].cells
-	hdr_cells[0].text = 'Network name'
-	hdr_cells[1].text = 'Speed'
-	hdr_cells[2].text = 'Port mode'
-	hdr_cells[3].text = 'IP Range'
-	hdr_cells[4].text = 'VLAN'
-	hdr_cells[5].text = 'Interface'
+   hdr_cells = table.rows[0].cells
+   hdr_cells[0].text = 'Network name'
+   hdr_cells[1].text = 'Speed'
+   hdr_cells[2].text = 'Port mode'
+   hdr_cells[3].text = 'IP Range'
+   hdr_cells[4].text = 'VLAN'
+   hdr_cells[5].text = 'Interface'
 
-	for netCounter, network in enumerate(networkData):
-		networkRow = table.rows[netCounter+1].cells
-		networkRow[0].text = network['network_name']
-		networkRow[1].text = network['speed']
-		networkRow[2].text = network['port_mode']
-		networkRow[3].text = network['ip_range']
-		networkRow[4].text = network['vlan']
-		networkRow[5].text = network['interface']
+   for netCounter, network in enumerate(networkData):
+      networkRow = table.rows[netCounter+1].cells
+      networkRow[0].text = network['network_name']
+      networkRow[1].text = network['speed']
+      networkRow[2].text = network['port_mode']
+      networkRow[3].text = network['ip_range']
+      networkRow[4].text = network['vlan']
+      networkRow[5].text = network['interface']
 
-	doc.save('docs/4.network.docx')
+   doc.save('docs/4.network.docx')
 
 # Generate the token for access:
 try:
-	token = get_token()
+   token = get_token()
 except:
-	print("Could not generate access token - some doc builds may fail")
+   print("Could not generate access token - some doc builds may fail")
 
 # 1.cover
 try:
-	entries = json.load(open("entries.json"))
-	entries['DATE'] = time.strftime("%d %B, %Y")
-	docx_replace("templates/1.cover.docx","docs/1.cover.docx",entries)
-	print("Built docs/1.cover.docx")
+   entries = json.load(open("entries.json"))
+   entries['DATE'] = time.strftime("%d %B, %Y")
+   docx_replace("templates/1.cover.docx","docs/1.cover.docx",entries)
+   print("Built docs/1.cover.docx")
 except:
-	print("Failed to build docs/1.cover.docx")
+   print("Failed to build docs/1.cover.docx")
 
 # 2.intro
 
@@ -183,42 +227,42 @@ except:
 # 4.network
 # Some network info available on REST: /api/clusters/<cluster number>/network_configuration/
 try:
-	gen_network_layout_table(network_info())
-	print("Built docs/4.network.docx")
+   gen_network_layout_table(network_info())
+   print("Built docs/4.network.docx")
 except:
-	print("Failed to build docs/4.network.docx")
+   print("Failed to build docs/4.network.docx")
 
 # 5.nodes
 try:
-	gen_nodes_table(get_nodes(token))
-	print("Built docs/5.nodes.docx")
+   gen_nodes_table(get_nodes(token))
+   print("Built docs/5.nodes.docx")
 except:
-	print("Failed to build docs/5.nodes.docx")
+   print("Failed to build docs/5.nodes.docx")
 
 # 6.access
-try:
-	fuel = fuel_info()
-	access_replace = {
-	"FUELURL": fuel['url'],
-	"FUELIP": args.host,
-	"WEBUSER": fuel['web']['username'],
-	"WEBPW": fuel['web']['password'],
-	"HORURL": fuel['horizon'],
-	"SSHUSER": fuel['ssh']['username'],
-	"SSHUSER": fuel['ssh']['username'],
-	"SSHPW": fuel['ssh']['password']
-	}
-	docx_replace("templates/6.access.docx","docs/6.access.docx",access_replace)
-	print("Built docs/6.access.docx")
-except:
-	print("Failed to build docs/6.access.docx")
+fuel = fuel_info()
+gen_access_table(fuel)
+'''
+access_replace = {
+"FUELURL": fuel['url'],
+"FUELIP": args.host,
+"WEBUSER": fuel['web']['username'],
+"WEBPW": fuel['web']['password'],
+"HORURL": fuel['horizon'],
+"SSHUSER": fuel['ssh']['username'],
+"SSHUSER": fuel['ssh']['username'],
+"SSHPW": fuel['ssh']['password']
+}
+docx_replace("templates/6.access.docx","docs/6.access.docx",access_replace)
+'''
+print("Built docs/6.access.docx")
 # 7.fuel
 try:
-	fuel_replace = {
-	"TOTAL_NODES" : len(nodes)	
-	}
-	docx_replace("templates/7.fuel.docx","docs/7.fuel.docx",fuel_replace)
-	print("Built docs/7.fuel.docx")
+   fuel_replace = {
+   "TOTAL_NODES" : len(nodes) 
+   }
+   docx_replace("templates/7.fuel.docx","docs/7.fuel.docx",fuel_replace)
+   print("Built docs/7.fuel.docx")
 except:
-	print("Failed to build docs/7.fuel.docx")
+   print("Failed to build docs/7.fuel.docx")
 
