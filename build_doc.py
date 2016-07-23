bad0694c075177f1edaaff111c630465b207533c
Matt Schafer <mschafer@mirantis.com>
2016-07-22 19:18:38 -0700

    Starting change to remove interfaces from network table and cross-reference speed with nodes data

--- build_doc.py
+++ build_doc.py
@@ -67,10 +67,10 @@ def network_info(cluster_id):
     for x in network_data['networks']:
         network = {}
         network['network_name'] = str(x['name']) if x['name'] is not None else 'no data'
-        network['speed'] = 'no data'
+        network['speed'] = str('no data'
         network['port_mode'] = 'no data'
-        network['ip_range'] = str(x['cidr']) if x['name'] is not None else 'no data'# Check this - might be ['networks'][x]['ip_ranges'] instead
-        network['vlan'] = str(x['vlan_start']) if x['vlan_start'] is not None else 'no data'
+        network['ip_range'] = str(x['cidr']) if x['name'] is not None else 'no data'
+        network['vlan'] = str(x['vlan_start']) if x['vlan_start'] is not None else 'no data'# Check native
         network['interface'] = 'no data'
         network['gateway'] = str(x['gateway']) if x['gateway'] is not None else 'no data'
         networks.append(network)
@@ -280,14 +280,14 @@ def gen_nodes_table(nodeData):
       nodeRow[5].text = [str(x['disk']) + ': ' + str(int(x['size']/1073741824)) + 'GB \n' for x in nodeData[nodeCounter]['meta']['disks']]
 
 # Gathers network information on the cluster in question using the REST interface
-def network_info(cluster_id):
+def network_info(cluster_id, node_data):
     header = {'X-Auth-Token': token,'Content-Type': 'application/json'}
     network_data = json.loads(requests.get(url='https://' + args.host + ':' + args.web_port + '/api/clusters/' + str(cluster_id) + '/network_configuration/neutron/', headers=header, verify=False).text)
     networks = []
     for x in network_data['networks']:
         network = {}
         network['network_name'] = str(x['name']) if x['name'] is not None else 'no data'
-        network['speed'] = 'no data'
+        network['speed'] = 'no data'#str(node_data[x]['meta']['interfaces'][y]['max_speed']['current_speed'])
         network['port_mode'] = 'no data'
         network['ip_range'] = str(x['cidr']) if x['name'] is not None else 'no data'# Check this - might be ['networks'][x]['ip_ranges'] instead
         network['vlan'] = str(x['vlan_start']) if x['vlan_start'] is not None else 'no data'
@@ -315,7 +315,6 @@ def gen_network_layout_table(networkData, env):
     hdr_cells[2].text = 'Port mode'
     hdr_cells[3].text = 'IP Range'
     hdr_cells[4].text = 'VLAN'
-    hdr_cells[5].text = 'Interface'
 
     for netCounter, network in enumerate(networkData):
         networkRow = table.rows[netCounter+1].cells
@@ -323,9 +322,7 @@ def gen_network_layout_table(networkData, env):
         networkRow[1].text = network['speed']
         networkRow[2].text = network['port_mode']
         networkRow[3].text = network['ip_range']
-        networkRow[4].text = network['vlan']
-        networkRow[5].text = network['interface']
-
+       # networkRow[4].text = network['vlan']
 
     runbook.add_page_break()
 
@@ -521,7 +518,7 @@ runbook.add_page_break()
 for cluster in clusters:
     results = re.search('(https|http):\/\/.+\/(\d+)',cluster)
     cluster_id = results.group(2)
-    gen_network_layout_table(network_info(str(cluster_id)),cluster_id)
+    gen_network_layout_table(network_info(str(cluster_id)),cluster_id, nodedata)
 
 runbook.add_page_break()
 runbook.add_page_break()
